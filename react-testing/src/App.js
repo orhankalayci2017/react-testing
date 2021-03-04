509a22ad95b24a1c549c06efc43a35ba287913b7
orhankalayci-devopscu <orhan@kalayci.com>
2021-03-04 07:29:46 +0300

    change the code

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    isActive: false
+  }
+
+  handleClick = () => {
+    this.setState({ isActive: !this.state.isActive })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <button>Hide</button>
+        <button onClick={this.handleClick}>
+          {this.props.show ? "Hide" : "Show"}
+        </button>
       </div>
     );
   }
